1e232280dd9880f13495de4a6c116071923145c0
Jonathing <me@jonathing.me>
2025-02-26 18:47:40 -0500

    Fix the task creation method still using ConfigureUtil

--- src/main/groovy/org/cadixdev/gradle/licenser/LicenseExtension.groovy
+++ src/main/groovy/org/cadixdev/gradle/licenser/LicenseExtension.groovy
@@ -30,7 +30,6 @@ import org.gradle.api.Action
 import org.gradle.api.Incubating
 import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
-import org.gradle.api.internal.ConfigureByMapAction
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.ExtraPropertiesExtension
 import org.gradle.api.provider.ListProperty
@@ -39,8 +38,6 @@ import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.resources.TextResourceFactory
 import org.gradle.api.tasks.util.PatternFilterable
 import org.gradle.api.tasks.util.PatternSet
-import org.gradle.util.ConfigureUtil
-import org.jetbrains.annotations.ApiStatus
 
 import javax.inject.Inject
 
